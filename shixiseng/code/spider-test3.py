d66c1408398551e223d21f453419ba306ee00332
Zhimin7 <guzhimin7@gmail.com>
2021-11-22 20:42:49 +0800

    实习僧

--- shixiseng/code/spider-test3.py
+++ shixiseng/code/spider-test3.py
@@ -8,4 +8,19 @@ from fontTools.ttLib import TTFont
 url = 'http://www.porters.vip/confusion/movie.html'
 resp = requests.get(url)
 sel = Selector(resp.text)
-css_path = sel.css('link[re=stylesheet]')
+css_path = sel.css('link[rel=stylesheet]::attr(href)').extract()
+print(css_path)
+woffs = []
+for c in css_path:
+    css_url = parse.urljoin(url, c)
+    css_resp = requests.get(css_url)
+    woff_path = re.findall("src:url\('..(.*.woff)'\) format\('woff'\);", css_resp.text)
+    print(woff_path)
+    if woff_path:
+        woffs += woff_path
+print(woffs)
+woff_url = 'http://www.porters.vip/confusion/' + woffs.pop()
+woff = requests.get(woff_url)
+filename = './woff/target.woff'
+with open(filename, 'wb') as f:
+    f.write(woff.content)
